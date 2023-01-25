a3845bfcd715cfab772746dd2380da18c775a90c
pelzfx <111582303+pelzfx@users.noreply.github.com>
2023-01-25 13:06:17 +0100

    first commit

---
+++ rewards.js
@@ -0,0 +1,5 @@
+var link = document.getElementById("myLink");
+link.onclick = function() {
+    var promptValue = prompt("Enter Mobile Number to recieve your airtime");
+    // do something with promptValue
+};
