7a2dd150f7ebe461a6d0b26e85530f51c842284e
0xZaddyy <oyinoladapo5@gmail.com>
2023-01-26 10:57:34 +0100

    oyin's changes

--- rewards.js
+++
@@ -1,5 +0,0 @@
-var link = document.getElementById("myLink");
-link.onclick = function() {
-    var promptValue = prompt("Enter Mobile Number to recieve your airtime");
-    // do something with promptValue
-};
